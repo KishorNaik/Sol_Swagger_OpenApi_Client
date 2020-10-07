02ad5c885eb90b1463a8056da91863006fca9e10
Kishor Naik <kishor.naik011.net@outlook.com>
2020-10-07 18:24:36 +0530

    Code Commit

---
+++ Sol_Demo/Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var httpClient = new HttpClient();
+            var swaggerClient = new swaggerClient("http://localhost:17225/", httpClient);
+
+            var getCustomerList = await swaggerClient?.GetcustomerAsync();
+
+            foreach (CustomerModel customerModel in getCustomerList)
+            {
+                Console.WriteLine(customerModel.FullName);
+            }
+
+            var flag = await swaggerClient?.AddAsync(new CustomerModel()
+            {
+                FullName = "Kishor Naik",
+                EmailId = "kishor.naik011.net@gmail.com",
+                MobileNo = "9111111111"
+            });
+
+            Console.WriteLine(flag);
+        }
+    }
+}
